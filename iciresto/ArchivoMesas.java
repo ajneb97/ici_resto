d038169e747806e0178dff1d2eba9f1e8d0e8570
Claudia997 <30850291+Claudia997@users.noreply.github.com>
2017-11-21 08:57:23 -0300

    Corrección
    -Actualización de directorio

--- iciresto/ArchivoMesas.java
+++ iciresto/ArchivoMesas.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 public class ArchivoMesas extends Archivo{
 
     public ArchivoMesas() {
-        super("mesas.txt");
+        super("recursos/mesas.txt");
     }  
         
     @Override
