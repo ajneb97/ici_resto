bba57704c3f8702d0ffb944649b96b83c3628090
Claudia997 <30850291+Claudia997@users.noreply.github.com>
2017-12-03 14:16:34 -0300

    Update Login.java

--- iciresto/Login.java
+++ iciresto/Login.java
@@ -1,28 +1,43 @@
 package iciresto;
 
-
 import java.util.ArrayList;
 
 import archivos.ArchivoLog;
-
+/*
+ * @author C.Garay,H.Sepúlveda,B.Lebrecht
+ * @version November 8th 2017
+ */
 public class Login {
     private String username;
     private String password;
     
+   /**
+    *  
+    * @param username String Nombre de usuario ingresado
+    * @param password String Contraseña de usuario ingresado
+    */
     public Login(String username, String password){
         this.username = username;
         this.password = password;
     }
+    
+    /**
+     * Clase destinada a cada login, con username y password.
+     * @return boolean True si el usuario existe en el archivo de registro de usuario, False de lo contrario
+     */
    public boolean usuarioExiste(){
         String usuario = this.username+";"+this.password;
      
         ArchivoLog archLog = new ArchivoLog();
-        ArrayList<String> listaUsuarios = archLog.leer();
-        for(int i=0; i<listaUsuarios.size(); i++){
-            if(usuario.equals(listaUsuarios.get(i))){
-                return true;                
-            }
+        if(archLog.esPrimeraVez()) {
+        	archLog.agregarTexto("admin;admin");
         }
+        ArrayList<String> listaUsuarios = archLog.listarContenido();
+        for(int i=0; i<listaUsuarios.size(); i++){
+          if(usuario.equals(listaUsuarios.get(i))){
+            return true;                
+      }
+      }
         return false;
     }
 }
